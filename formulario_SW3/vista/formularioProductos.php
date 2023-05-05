d7b73d6c242fec02d80d624a239efa684125625d
cehernandez <cehernandez@unicauca.edu.co>
2023-05-04 19:30:52 -0500

    CRUD en php

---
+++ formulario_SW3/vista/formularioProductos.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'modelo/clsProductoCRUD.php';
+$codigo = isset($_REQUEST['codigo']) ? $_REQUEST['codigo'] : '';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"> 
+        <title>Formulario De Registro</title>
+        <link rel="stylesheet" type="text/css" href="vista/css/estilo.css">
+    </head>
+    <body>
+	<div class="contenedor">
+		<form action="?c=Producto&a=Registrar" class="formulario" id="formulario" name="formulario" method="POST">
+			<div class="contenedor-inputs">
+				<input type="text" name="nombre" placeholder="Nombre">
+				<input type="text" name="precio" placeholder="Precio">
+				
+				<ul class="error" id="error"></ul>
+			</div>
+
+			<input type="submit" class="btn" name="registrarse" value="Registrate">
+		</form>
+	<script src="vista/scripts/formulario.js"></script>
+</body>
+</html>
